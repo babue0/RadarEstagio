45030449d19a489f896400448b8c8753435ae6be
babue0 <iandpcarvalho@gmail.com>
2026-08-31 20:47:39 -0300

    test(domain): atualiza habilidades esperadas do perfil fixo

--- tests/test_models.py
+++ tests/test_models.py
@@ -72,7 +72,7 @@ def test_perfil_rejeita_lista_de_habilidades_vazia():
 def test_perfil_do_mvp_e_valido():
     perfil = perfil_do_mvp()
     assert perfil.modalidade is Modalidade.PRESENCIAL
-    assert perfil.habilidades == ["Python", "Java"]
+    assert perfil.habilidades == ["Python", "Git", "Java", "SQL"]
 
 
 def test_usuario_exige_chat_id_preenchido():
